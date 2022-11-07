be7a6ebb25e83078d1a16fa652de6e1d7bd57127
carloshnp <carloshnpwork@gmail.com>
2022-11-07 13:18:47 -0300

    feat: post tweets

--- src/app.js
+++ src/app.js
@@ -11,6 +11,10 @@ const users = [
     avatar:
       "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
   },
+  {
+    username: "gato",
+    avatar: "https://www.larazon.es/resizer/6ZgsBmWHenWo-kxfXbG3DLl0j_g=/600x400/smart/filters:format(jpg)/cloudfront-eu-central-1.images.arcpublishing.com/larazon/WDD4YYSISFGDRJPKHYGRUIRTRU.jpg"
+  }
 ];
 
 const tweets = [
@@ -18,13 +22,17 @@ const tweets = [
     username: "bobesponja",
     tweet: "eu amo o hub",
   },
+  {
+    username: "gato",
+    tweet: "miau"
+  }
 ];
 
 server.post("/sign-up", (req, ans) => {
   const { username, avatar } = req.body;
   const user = {
-    username: username,
-    avatar: avatar,
+    username,
+    avatar,
   };
   users.push(user);
   ans.send("OK");
@@ -45,8 +53,18 @@ server.get("/tweets", (req, ans) => {
       const avatarTweet = { ...tweet, avatar: find.avatar };
       return avatarTweet;
     }
-  );
+  ).reverse();
   ans.send(avatarTweets);
 });
+
+server.post('/tweets', (req, ans) => {
+  const {username, tweet} = req.body
+  const tweetPost = {
+    username,
+    tweet
+  }
+  tweets.push(tweetPost)
+  ans.send('OK')
+})
 
 server.listen(5000);
